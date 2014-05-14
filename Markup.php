cf9a74f74040e8e16ad537b1c4bcd48394a8a8f7
Md Abdullah Al Maruf <maruf.sylhet@gmail.com>
2014-05-14 21:35:19 +0100

    Update Markup.php

--- Markup.php
+++ Markup.php
@@ -1,48 +1,48 @@
 <?php
-class Zend_View_Helper_Markup{
+class Markup{
 
 private $number = 1;
 
 /*
 * Image function should be used for any anchor link
 */
-public function image($src){
-  return "<img src='$src'/>";
-}
+  public function image($src){
+      return "<img src='$src'/>";
+  }
 
 /*
 * Menu function should be used for any anchor link
 */
-public function menu($text, $href, null, null, $letter = null){
-  $number = 1;
-  if($letter){
-    $a = $letter;
-  }else{
-    $a = $this->number; $this->number++;
+  public function menu($text, $href, null, null, $letter = null){
+      $number = 1;
+      if($letter){
+        $a = $letter;
+      }else{
+        $a = $this->number; $this->number++;
+      }
+      return "<a href='$href'>$a</a>) $text";
   }
-  return "<a href='$href'>$a</a>) $text";
-}
 
 /*
 * h1 function should be used for Slogan 
 */
-public function h1($text){
-  return "<h1>$text</h1>";
-}
+  public function h1($text){
+      return "<h1>$text</h1>";
+  }
 
 
 /*
 * title function should be used for Slogan 
 */
-public function title($text){
-  return "<p style='text-align:center;'>$text</p>";
-}
+  public function title($text){
+      return "<p style='text-align:center;'>$text</p>";
+  }
 
 
 /*
 * text function should be used for any body text 
 */
-public function text($text){
-  return "<p>$text</p>";
-}
+  public function text($text){
+    return "<p>$text</p>";
+  }
 }
