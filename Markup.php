a852f3f6e2182f723b62ca6baff97af4c6454db9
Md Abdullah Al Maruf <maruf.sylhet@gmail.com>
2014-05-14 20:42:13 +0100

    Create Markup.php

---
+++ Markup.php
@@ -0,0 +1,48 @@
+<?php
+class Zend_View_Helper_Markup{
+
+private $number = 1;
+
+/*
+* Image function should be used for any anchor link
+*/
+public function image($src){
+  return "<img src='$src'/>";
+}
+
+/*
+* Menu function should be used for any anchor link
+*/
+public function menu($text, $href, null, null, $letter = null){
+  $number = 1;
+  if($letter){
+    $a = $letter;
+  }else{
+    $a = $this->number; $this->number++;
+  }
+  return "<a href='$href'>$a</a>) $text";
+}
+
+/*
+* h1 function should be used for Slogan 
+*/
+public function h1($text){
+  return "<h1>$text</h1>";
+}
+
+
+/*
+* title function should be used for Slogan 
+*/
+public function title($text){
+  return "<p style='text-align:center;'>$text</p>";
+}
+
+
+/*
+* text function should be used for any body text 
+*/
+public function($text){
+  return "<p>$text</p>";
+}
+}
