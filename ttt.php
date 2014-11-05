b42a322d88616ed139b4bca91845b23bb5af5f1d
Md Abdullah Al Maruf <abdullah@every1mobile.net>
2014-11-05 17:02:51 +0000

    ttt working with basic functionality

--- ttt.php
+++ ttt.php
@@ -1,119 +1,206 @@
 <?php
 $t = new T();
 echo $t->getHtml();
-echo "Won - " . var_dump($t->check());
+echo "Won - " . $t->winner();
+echo "<a href='?'>new</a>";
 
-class T{
-    const GAME_NAME = 'TTT';
-    const USER_VALUE = 1;
-    const SYS_VALUE = 2;
+class T {
+    const GAME_NAME = 'TTT';    
+    const SYS_SIGN = 'X';
+    const USER_SIGN = 'O';
+    
     private $_s;
+    
     private $_template = array(
         'a1' => null, 'a2' => null, 'a3' => null,
         'b1' => null, 'b2' => null, 'b3' => null,
         'c1' => null, 'c2' => null, 'c3' => null,
     );
 
-    public function __construct($options = null){
-        session_start();            
-        if(! isset($_GET['ur'])){
+    public function __construct($options = null) {
+        session_start();
+        
+        if (! isset($_GET['ur'])) {            
             $_SESSION[ self::GAME_NAME ] = $this->_template;
-        }else{
-            $index = $_GET['ur'];
-            if(null === $_SESSION[ self::GAME_NAME ][ $index ]){
-                $_SESSION[ self::GAME_NAME ][ $index ] = 1;
+            $this->_s = $_SESSION[ self::GAME_NAME ];
+        } else {
+            $index = $_GET['ur'];            
+            if (null === $_SESSION[ self::GAME_NAME ][ $index ]) {
+                $_SESSION[ self::GAME_NAME ][ $index ] = $this->getUserSign();
             }
-        }
 
-        $this->_s = $_SESSION[ self::GAME_NAME ];
+            $this->_s = $_SESSION[ self::GAME_NAME ];
+            $this->_systemResponse();
+        }
+        
+        // var_dump($this->_s);
     }
 
-/*
+    /*
     a1 a2 a3
     a1 b2 c3
     a1 b1 c1
-    a2 b2 c2 
+    a2 b2 c2
     a3 b3 c3
-    a3 b2 c1 
+    a3 b2 c1
     b1 b2 b3
     c1 c2 c3
  */
-    private function tier1Move($sysOrUser){
+    private function tier2Move($sysOrUser, $toPut) {
         if($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['a2'] && null === $this->_s['a3']){
-            $this->_s['a3'] = self::SYS_VALUE; return true;
+            $this->_s['a3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a2'] && $this->_s['a2'] == $this->_s['a3'] && null === $this->_s['a1']){
-            $this->_s['a1'] = self::SYS_VALUE; return true;
+            $this->_s['a1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['a3'] && null === $this->_s['a2']){
-            $this->_s['a2'] = self::SYS_VALUE; return true;        
+            $this->_s['a2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['b2'] && null === $this->_s['c3']){
-            $this->_s['c3'] = self::SYS_VALUE; return true;
+            $this->_s['c3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b2'] && $this->_s['b2'] == $this->_s['c3'] && null === $this->_s['a1']){
-            $this->_s['a1'] = self::SYS_VALUE; return true;
+            $this->_s['a1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['c3'] && null === $this->_s['b2']){
-            $this->_s['b2'] = self::SYS_VALUE; return true;        
+            $this->_s['b2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['b1'] && null === $this->_s['c1']){
-            $this->_s['c1'] = self::SYS_VALUE; return true;
+            $this->_s['c1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b1'] && $this->_s['b1'] == $this->_s['c1'] && null === $this->_s['a1']){
-            $this->_s['a1'] = self::SYS_VALUE; return true;
+            $this->_s['a1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a1'] && $this->_s['a1'] == $this->_s['c1'] && null === $this->_s['b1']){
-            $this->_s['b1'] = self::SYS_VALUE; return true;        
+            $this->_s['b1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a2'] && $this->_s['a2'] == $this->_s['b2'] && null === $this->_s['c2']){
-            $this->_s['c2'] = self::SYS_VALUE; return true;
+            $this->_s['c2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b2'] && $this->_s['b2'] == $this->_s['c2'] && null === $this->_s['a2']){
-            $this->_s['a2'] = self::SYS_VALUE; return true;
+            $this->_s['a2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a2'] && $this->_s['a2'] == $this->_s['c2'] && null === $this->_s['b2']){
-            $this->_s['b2'] = self::SYS_VALUE; return true;
+            $this->_s['b2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a3'] && $this->_s['a3'] == $this->_s['b3'] && null === $this->_s['c3']){
-            $this->_s['c3'] = self::SYS_VALUE; return true;
+            $this->_s['c3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b3'] && $this->_s['b3'] == $this->_s['c3'] && null === $this->_s['a3']){
-            $this->_s['a3'] = self::SYS_VALUE; return true;
+            $this->_s['a3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a3'] && $this->_s['a3'] == $this->_s['c3'] && null === $this->_s['b3']){
-            $this->_s['b3'] = self::SYS_VALUE; return true;        
+            $this->_s['b3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a3'] && $this->_s['a3'] == $this->_s['b2'] && null === $this->_s['c1']){
-            $this->_s['c1'] = self::SYS_VALUE; return true;
+            $this->_s['c1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b2'] && $this->_s['b2'] == $this->_s['c1'] && null === $this->_s['a3']){
-            $this->_s['a3'] = self::SYS_VALUE; return true;
+            $this->_s['a3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['a3'] && $this->_s['a3'] == $this->_s['c1'] && null === $this->_s['b2']){
-            $this->_s['b2'] = self::SYS_VALUE; return true;
+            $this->_s['b2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b1'] && $this->_s['b1'] == $this->_s['b2'] && null === $this->_s['b3']){
-            $this->_s['b3'] = self::SYS_VALUE; return true;
+            $this->_s['b3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b2'] && $this->_s['b2'] == $this->_s['b3'] && null === $this->_s['b1']){
-            $this->_s['b1'] = self::SYS_VALUE; return true;
+            $this->_s['b1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['b1'] && $this->_s['b1'] == $this->_s['b3'] && null === $this->_s['b2']){
-            $this->_s['b2'] = self::SYS_VALUE; return true;
+            $this->_s['b2'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['c1'] && $this->_s['c1'] == $this->_s['c2'] && null === $this->_s['c3']){
-            $this->_s['c3'] = self::SYS_VALUE; return true;
+            $this->_s['c3'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['c2'] && $this->_s['c2'] == $this->_s['c3'] && null === $this->_s['c1']){
-            $this->_s['c1'] = self::SYS_VALUE; return true;
+            $this->_s['c1'] = $toPut; return true;
         }elseif($sysOrUser == $this->_s['c1'] && $this->_s['c1'] == $this->_s['c3'] && null === $this->_s['c2']){
-            $this->_s['c2'] = self::SYS_VALUE; return true;
+            $this->_s['c2'] = $toPut; return true;
         }
 
         $_SESSION[ self::GAME_NAME ] = $this->_s;
-   }
-
-    public function tier2MoveForking(){
-
+    }
+    
+    public function tier1Move() {
+        
     }
 
-    public function systemResponse(){
-        $moveMade = false;
-
-        // 1. Take a winning move
-        $moveMade = $this->tier1Move($sysOrUser);
+    private function _systemResponse() {
+        // 1. Take a winning move        
+        if ($this->tier2Move($this->getSysSign(), $this->getSysSign())) {
+            return true;
+        }
         
         // 2. Block Opponent winning move
-        if(false === $moveMade){
-            $moveMade = $this->tier1Move(self::USER_VALUE);
+        if ($this->tier2Move($this->getUserSign(), $this->getSysSign())) {
+            return true;
+        }
+        
+        // 3. make a primary move
+        if ($this->tier0Move()) {
+            return true;
+        }
+        die('failed to put a system response');
+    }
+    
+    public function tier0Move($level = 'advanced') {
+        $tmp = $this->_template;
+        
+        // get rid of the indexes which have been taken up
+        foreach ($this->_s as $k => $v) {
+            if (null !== $v) {
+                unset($tmp[ $k ]);
+            }
+        }
+        
+        // See assign system sign on a smart index
+        $smartIndices = array('a1' => null, 'a3' => null, 'b2' => null, 'c1' => null, 'c3' => null);                
+        $smartIndicesLeft = array_intersect_assoc($tmp, $smartIndices);        
+        if (! empty($smartIndicesLeft)) {
+            $rand = array_rand($smartIndicesLeft);
+            $this->_s[ $rand ] = $this->getSysSign();
+            $_SESSION[ self::GAME_NAME ] = $this->_s;
+            return true;
+        }
+        
+        // Finally assign system sign on a non-smart index
+        $nonSmartIndices = array('a2' => null, 'b1' => null, 'b3' => null, 'c2' => null);
+        $nonSmartIndicesLeft = array_intersect_assoc($tmp, $nonSmartIndices);
+        if (! empty($nonSmartIndicesLeft)) {
+            $rand = array_rand($nonSmartIndicesLeft);
+            $this->_s[ $rand ] = $this->getSysSign();
+            $_SESSION[ self::GAME_NAME ] = $this->_s;
+            return true;
+        }
+    }
+    
+    public function winner() {
+        if(null !== $this->_s['a1']
+            && $this->_s['a1'] == $this->_s['a2']
+            && $this->_s['a2'] ==  $this->_s['a3']){
+                return $this->_s['a1'];
+
+        }elseif(null !== $this->_s['a1']
+            && $this->_s['a1'] == $this->_s['b2']
+            && $this->_s['b2'] ==  $this->_s['c3']){
+                return $this->_s['a1'];
+
+        }elseif(null !== $this->_s['a1']
+            && $this->_s['a1'] == $this->_s['b1']
+            && $this->_s['b1'] ==  $this->_s['c1']){
+                return $this->_s['a1'];
+
+        }elseif(null !== $this->_s['a2']
+            && $this->_s['a2'] == $this->_s['b2']
+            && $this->_s['b2'] ==  $this->_s['c2']){
+                return $this->_s['a2'];
+
+        }elseif(null !== $this->_s['a3']
+            && $this->_s['a3'] == $this->_s['b3']
+            && $this->_s['b3'] ==  $this->_s['c3']){
+                return $this->_s['a3'];
+
+        }elseif(null !== $this->_s['a3']
+            && $this->_s['a3'] == $this->_s['b2']
+            && $this->_s['b2'] ==  $this->_s['c1']){
+                return $this->_s['a3'];
+
+        }elseif(null !== $this->_s['b1']
+            && $this->_s['b1'] == $this->_s['b2']
+            && $this->_s['b2'] ==  $this->_s['b3']){
+                return $this->_s['b1'];
+
+        }elseif(null !== $this->_s['c1']
+            && $this->_s['c1'] == $this->_s['c2']
+            && $this->_s['c2'] ==  $this->_s['c3']){
+               return $this->_s['c1'];
         }
     }
 
-
-    public function getHtml(){
+    public function getHtml() {
         $html = "<table>";
         foreach($this->_s as $k => $v){
             if($k == 'a1' || $k == 'b1' || $k == 'c1'){
-                $html .= "<tr>"; 
+                $html .= "<tr>";
             }
 
             $ref = "<a href='?ur=$k'>$k</a>";
@@ -125,73 +212,20 @@ class T{
         }
         return $html .= "</table>";
     }
-
-    public function winner(){
-        if(null !== $this->_s['a1'] 
-            && $this->_s['a1'] == $this->_s['a2'] 
-            && $this->_s['a2'] ==  $this->_s['a3']){
-                return $this->_s['a1'];
-
-        }elseif(null !== $this->_s['a1'] 
-            && $this->_s['a1'] == $this->_s['b2']
-            && $this->_s['b2'] ==  $this->_s['c3']){
-                return $this->_s['a1'];       
-
-        }elseif(null !== $this->_s['a1'] 
-            && $this->_s['a1'] == $this->_s['b1']
-            && $this->_s['b1'] ==  $this->_s['c1']){
-                return $this->_s['a1'];
-
-        }elseif(null !== $this->_s['a2'] 
-            && $this->_s['a2'] == $this->_s['b2']
-            && $this->_s['b2'] ==  $this->_s['c2']){
-                return $this->_s['a2'];
-
-        }elseif(null !== $this->_s['a3'] 
-            && $this->_s['a3'] == $this->_s['b3'] 
-            && $this->_s['b3'] ==  $this->_s['c3']){
-                return $this->_s['a3'];       
-
-        }elseif(null !== $this->_s['a3'] 
-            && $this->_s['a3'] == $this->_s['b2'] 
-            && $this->_s['b2'] ==  $this->_s['c1']){
-                return $this->_s['a3'];
-
-        }elseif(null !== $this->_s['b1'] 
-            && $this->_s['b1'] == $this->_s['b2'] 
-            && $this->_s['b2'] ==  $this->_s['b3']){
-                return $this->_s['b1'];
-
-        }elseif(null !== $this->_s['c1'] 
-            && $this->_s['c1'] == $this->_s['c2']
-            && $this->_s['c2'] ==  $this->_s['c3']){
-               return $this->_s['c1'];
-        }
+    
+    public function setUserSign($value) {
+        $this->_s['user_sign'] = $value;
+        return $this;
+    }    
+    public function getUserSign() {
+        return (isset($this->_s['user_sign']) ? $this->_s['user_sign'] : self::USER_SIGN);
     }
-
-    public function check(){
-        $winner = $this->winner();
-
-        if($winner){
-            return $winner;
-        }else{
-        }
+    
+    public function setSysSign($value) {
+        $this->_s['sys_sign'] = $value;
+        return $this;
+    }    
+    public function getSysSign() {
+        return (isset($this->_s['sys_sign']) ? $this->_s['sys_sign'] : self::SYS_SIGN);
     }
 }
-
-/*
-a1 a2 a3 
-b1 b2 b3
-c1 c2 c3    
-
-            a1 a2 a3
-            a1 b2 c3
-            a1 b1 c1
-            a2 b2 c2 
-            a3 b3 c3
-            a3 b2 c1 
-            b1 b2 b3
-            c1 c2 c3
- 
-        */
-
